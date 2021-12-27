33db6168583725542bfd674e4093aab401df5998
Arfameher <arfameher@gmail.com>
2021-12-27 16:40:55 +0100

    Changes done

--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 from flask_navigation import Navigation
 import sqlite3
 import os
-#import plotly
-#import plotly.express as px
+import json
+import pandas as pd
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
   
 app = Flask(__name__)
 nav = Navigation(app)
@@ -12,6 +15,7 @@ nav = Navigation(app)
 nav.Bar('top', [
     nav.Item('Home', 'table'),
     nav.Item('Insights', 'image'),
+    nav.Item('Interactive', 'chart')
 ])
 
 def get_db_connection():
@@ -34,6 +38,21 @@ def table():
 def image():
     return render_template('image.html')
   
+    
+@app.route('/chart')
+def chart():
+
+    # Graph One
+    df = pd.read_csv("Dataset/clean_data.csv")
+    
+    labels = df['platforms'].unique()
+    values = df['platforms'].value_counts()
+
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+
+    graph1JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index1.html', title="home", graph1JSON=graph1JSON)
 
 if __name__ == '__main__':
     port = os.environ.get("PORT")
